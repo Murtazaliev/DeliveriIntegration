834ed2378b02b113ce291deab098408ded3052f2
Akhmed <kamaludinov@cron.ooo>
2021-09-09 17:06:03 +0300

    Привел ContentType к полям из класса с константами

--- Concrete/Repository.cs
+++ Concrete/Repository.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web;
+using Delivery.SelfServiceKioskApi.Helpers;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -20,14 +21,37 @@ namespace Delivery.SelfServiceKioskApi.Concrete
             _baseUrl = baseUrl;
         }
         
-        public async Task<string> GetAsync<T>(string method, T data)
+        public async Task<string> GetAsync<T>(string method, T data, string token)
         {
             try
             {
                 var parameters = ToQueryString(data); 
                 HttpWebRequest webRequest = WebRequest.CreateHttp($"{_baseUrl}{method}?{parameters}");
                 webRequest.Method = "GET";
-                webRequest.ContentType = "Application/json";
+                webRequest.ContentType = ContentTypes.ApplicationJson;
+                if(token != string.Empty)
+                    webRequest.Headers.Add(HttpRequestHeader.Authorization, token);
+                
+                var mainResponse = await ReadResponseAsync(webRequest.GetResponse());
+                return mainResponse;
+            }
+            catch (WebException webException)
+            {
+                var mainResponse = await ReadResponseAsync(webException.Response);
+                return mainResponse;
+            }
+        }
+        
+        public async Task<string> GetAsync(string method, string token)
+        {
+            try
+            {
+                HttpWebRequest webRequest = WebRequest.CreateHttp($"{_baseUrl}{method}");
+                webRequest.Method = "GET";
+                webRequest.ContentType = ContentTypes.ApplicationJson;
+                if(token != string.Empty)
+                    webRequest.Headers.Add(HttpRequestHeader.Authorization, token);
+                
                 var mainResponse = await ReadResponseAsync(webRequest.GetResponse());
                 return mainResponse;
             }
@@ -38,17 +62,19 @@ namespace Delivery.SelfServiceKioskApi.Concrete
             }
         }
 
-        public async Task<string> PostAsync<T>(string method, T data, string contentType)
+        public async Task<string> PostAsync<T>(string method, T data, string contentType, string token)
         {
             try
             {
                 var webRequest = WebRequest.CreateHttp(_baseUrl + method);
                 webRequest.Method = "POST";
                 webRequest.ContentType = contentType;
+                if (token != string.Empty)
+                    webRequest.Headers.Add(HttpRequestHeader.Authorization, token);
                 
-                if(contentType == "Application/json")
+                if(contentType == ContentTypes.ApplicationJson)
                     await WriteJsonBodyAsync(webRequest, data);
-                if (contentType == "form-data")
+                if (contentType == ContentTypes.FormData)
                     await WriteFormBodyAsync(webRequest, data);
                 
                 var mainResponse = await ReadResponseAsync(webRequest.GetResponse());
@@ -65,22 +91,22 @@ namespace Delivery.SelfServiceKioskApi.Concrete
         private async Task<string> ReadResponseAsync(WebResponse webResponse)
         {
             var httpWebResponse = ((HttpWebResponse) webResponse);
-            await using Stream responseStream = httpWebResponse.GetResponseStream();
-            using StreamReader responseReader = new StreamReader(responseStream);
+            await using var responseStream = httpWebResponse.GetResponseStream();
+            using var responseReader = new StreamReader(responseStream);
             return await responseReader.ReadToEndAsync();
         }
 
         private async Task WriteJsonBodyAsync<T>(HttpWebRequest webRequest, T writeBody)
         {
-            await using Stream requestStream = webRequest.GetRequestStream();
-            await using StreamWriter requestWriter = new StreamWriter(requestStream);
+            await using var requestStream = webRequest.GetRequestStream();
+            await using var requestWriter = new StreamWriter(requestStream);
             await requestWriter.WriteAsync(JsonConvert.SerializeObject(writeBody));
         }
         
         private async Task WriteFormBodyAsync<T>(HttpWebRequest webRequest, T writeBody)
         {
-            await using Stream requestStream = webRequest.GetRequestStream();
-            await using StreamWriter requestWriter = new StreamWriter(requestStream);
+            await using var requestStream = webRequest.GetRequestStream();
+            await using var requestWriter = new StreamWriter(requestStream);
             var data = ToQueryString(writeBody);
             await requestWriter.WriteAsync(data);
         }
