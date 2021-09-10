d04af7e933fae961605730389a553e3039cd823e
Akhmed <kamaludinov@cron.ooo>
2021-09-10 13:32:46 +0300

    Добавил модель организации Rkeeper

---
+++ Models/Rkeeper/RkeeperOrganizationModel.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Delivery.SelfServiceKioskApi.Models.Rkeeper
+{
+    public class RkeeperOrganizationModel
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("objectId")]
+        public int ObjectId { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("actualAddress")]
+        public string ActualAddress { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+    }
+}
