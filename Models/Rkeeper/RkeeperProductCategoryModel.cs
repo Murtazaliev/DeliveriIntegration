61d4ff7a4f08800d17a057a8e2b3ca4324a18cfc
Akhmed <kamaludinov@cron.ooo>
2021-09-10 13:29:21 +0300

    Добавил модель категории продукта Rkeeper

---
+++ Models/Rkeeper/RkeeperProductCategoryModel.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Delivery.SelfServiceKioskApi.Models.Rkeeper
+{
+    public class RkeeperProductCategoryModel
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("parentId")]
+        public string ParentId { get; set; }
+    }
+}
