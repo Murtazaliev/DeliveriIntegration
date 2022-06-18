60cb8cb71bfba9cea0402f24162ec6ca8bc75f15
Akhmed <kamaludinov@cron.ooo>
2022-06-18 12:38:23 +0300

    Модели для ас престижа

---
+++ Models/AsPrestige/AsPrestigeModels/AsPrestigeCategory.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Delivery.SelfServiceKioskApi.Models.AsPrestige.AsPrestigeModels
+{
+    public class AsPrestigeCategory
+    {
+        [JsonPropertyName("externalCategoryId")]
+        public string ExternalCategoryId { get; set; }
+
+        [JsonPropertyName("name_ru")]
+        public string NameRu { get; set; }
+    }
+}
---
+++ Models/AsPrestige/AsPrestigeModels/AsPrestigeProduct.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace Delivery.SelfServiceKioskApi.Models.AsPrestige.AsPrestigeModels
+{
+    public class AsPrestigeProduct
+    {
+        [JsonPropertyName("name_ru")]
+        public string NameRu { get; set; }
+
+        [JsonPropertyName("weight")]
+        public string Weight { get; set; }
+
+        [JsonPropertyName("externalCategoryId")]
+        public string ExternalCategoryId { get; set; }
+
+        [JsonPropertyName("externalId")]
+        public string ExternalId { get; set; }
+
+        [JsonPropertyName("Quantity")]
+        public int Quantity { get; set; }
+
+        [JsonProperty("cost")]
+        public int Cost { get; set; }
+
+        [JsonProperty("hidden")]
+        public bool Hidden { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+    }
+}
